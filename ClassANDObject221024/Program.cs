900cd1f5b7c7b8eba978aa5b398f0632ae9e02e0
Acer <Acer@DESKTOP-ABKHEEV>
2024-10-22 16:16:44 +0530

    Add project files.

---
+++ ClassANDObject221024/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using ClassANDObject221024Old;
+
+
+namespace ClassANDObject221024
+{
+    struct student
+    {
+       public int roll;
+        string name;
+        int total_marks;
+        string dept;
+    }
+    struct mystruct
+    {
+        //all variables
+        public int x;
+        public int y;
+
+
+        //methods
+        public void method1()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            student s1=new student();
+            s1.roll = 11;
+        }
+        
+    }
+}
